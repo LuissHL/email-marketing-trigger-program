c4c97d4af86a88031ba6e9adb22d788e3032966e
JohnnyAlysson <149841185+JohnnyAlysson@users.noreply.github.com>
2024-05-21 08:09:08 -0300

    create mailEditor.py and add test file emailInfo.json

---
+++ mailEditor.py
@@ -0,0 +1,30 @@
+#coletar info para email, será coletado de um json #Imaginar que o email vem em um json(dict) para o backend
+import json
+
+def getSubject():
+    openFile = open('emailInfo.json')
+    data = json.load(openFile)
+    email_subject =str(data["subject"])
+    openFile.close()
+    return email_subject
+def getBody():
+    openFile = open('emailInfo.json')
+    data = json.load(openFile)
+    email_body = str(data["body"])
+    openFile.close()
+    return email_body
+
+#escolher segmento # coletar da DB
+
+#agendar envio
+
+#previa do email
+
+#testing:
+if __name__ == "__main__":
+    print(getSubject())
+    print(getBody())
+
+
+
+
